703d3e39396335f98a997e037aa65e7e6382a36e
Gabriel Retcheski <gabriel.retcheski@pelissari.com>
2018-04-26 12:17:55 -0300

    Projeto rodando.

--- FarmSales/Layout/JS/Main.js
+++ FarmSales/Layout/JS/Main.js
@@ -17,8 +17,6 @@ app.config(['$routeProvider', function ($routeProvider) {
         .when("/solicitar", { templateUrl: "SubPages/Solicitar.html", controller: "PageCtrl" })
         .when("/perfil", { templateUrl: "SubPages/MeuPerfil.html", controller: "PageCtrl" })
         .when("/termosdeuso", { templateUrl: "SubPages/TermosDeUso.html", controller: "PageCtrl" })
-        .when("/login", { controller: "LoginController", templateUrl: "SubPages/Login/login.view.html", controllerAs: "vm" })
-        .when("/register", { controller: "RegisterController", templateUrl: "SubPages/Register/register.view.html", controllerAs: "vm" })
         // else 404
         .when("/404", { templateUrl: "SubPages/Errors/404.html", controller: "PageCtrl" })
         .otherwise("/404", { templateUrl: "SubPages/Errors/404.html", controller: "PageCtrl" });
@@ -34,98 +32,10 @@ app.config(['$locationProvider', function ($locationProvider) {
 app.controller('PageCtrl', function ($scope, $http, $rootScope, $modal, $sanitize, $document) {
 
     console.log("Page Controller reporting for duty.");
-
-    $scope.deliveries = [];
-    $scope.urgencyId = "1";
-    $scope.itemValue = "1";
-
-    $scope.userLoggedIn = $rootScope.globals.currentUser;
-
-    $scope.calculatePrice = function (routeTime, routeDistance, itemValue, urgencyId) {
-        // seta variáveis de preço base
-        var tarifaBase = 3;
-        var custoPorMin = 0.30;
-        var custoPorKm = 0.25;
-
-        var preco = tarifaBase + (custoPorMin * routeTime) + (custoPorKm * routeDistance);
-
-        // redefine preço de acordo com estimativa de valor do item
-        if (itemValue === 2) {
-            preco = preco * 1.2;
-        }
-        else if (itemValue === 3) {
-            preco = preco * 1.4;
-        }
-        else {
-            preco = preco;
-        }
-
-        // redefine preço de acordo com a urgência do pedido
-        if (urgencyId === 2) {
-            preco = preco * 1.3;
-        }
-        else if (urgencyId === 3) {
-            preco = preco * 1.5;
-        }
-        else {
-            preco = preco;
-        }
-        
-        $rootScope.routeTime = routeTime;
-        $rootScope.routeDistance = routeDistance;
-        $rootScope.preco = preco;
-        $scope.recebeOrigin = angular.element($document[0].querySelector('#origin'));
-        $scope.origin = $scope.recebeOrigin[0].value;
-        $scope.recebeDestination = angular.element($document[0].querySelector('#destination'));
-        $scope.destination = $scope.recebeDestination[0].value;
-        $rootScope.deliveryData = { "RouteTime": $scope.routeTime, "RouteDistance": $scope.routeDistance, "Price": $scope.preco, "UrgencyId": $scope.urgencyId, "PickupAddress": $scope.origin, "FinalAddress": $scope.destination }
-
-        return $scope.preco;
-    }
-
-    $scope.body = '<div><p><strong>Preço calculado:</strong> R$: ' + $scope.preco + ' </p></div>';
-
-    
-
-    $scope.showModal = function () {
-
-        $scope.calculatePrice(routeTime, routeDistance, $scope.itemValue, $scope.urgencyId);
-        $rootScope.Delivery = $rootScope.deliveryData;
-        $modal.open({
-            templateUrl: 'myModal.html',
-            controller: 'ModalCtrl',
-            resolve: {
-                body: function () {
-                    return $scope.body;
-                },
-                preco: function () {
-                    return $scope.preco;
-                }
-            }
-        });
-    }
-
-}).controller("ModalCtrl", function ($scope, $rootScope, $modalInstance, $http) {
-    $scope.Delivery = $rootScope.Delivery;
-    $scope.ok = function () {
-
-        $('#formPagSeguro').submit();
-
-        $http.post('http://localhost:58255/api/v1/deliveries/postbyuser', $scope.Delivery)
-            .then(function (response) {
-                alert(response);
-            });
-
-        $modalInstance.close();
-    };
-
-    $scope.cancel = function () {
-        $modalInstance.dismiss('cancel');
-    };
 });
 
 
-app.run(['$rootScope', '$location', '$cookies', '$http',
+/*app.run(['$rootScope', '$location', '$cookies', '$http',
     function run($rootScope, $location, $cookies, $http) {
         // keep user logged in after page refresh
         $rootScope.globals = $cookies.getObject('globals') || {};
@@ -141,7 +51,7 @@ app.run(['$rootScope', '$location', '$cookies', '$http',
                 $location.path("/login");
             }
         });
-    }]);
+    }]);*/
 
 /*progress bar*/
 app.controller("progressBar", function ($scope, $timeout, $http) {
@@ -162,52 +72,6 @@ app.controller("progressBar", function ($scope, $timeout, $http) {
     }, 200);
 
 });
-
-/*mapa do cadastro*/
-function initMap() {
-    var directionsDisplay = new google.maps.DirectionsRenderer;
-    var directionsService = new google.maps.DirectionsService;
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 14,
-        center: { lat: -25.44, lng: -49.35 }
-    });
-    directionsDisplay.setMap(map);
-
-    document.getElementById("submit_mapinfo").addEventListener('click', function () {
-        calculateAndDisplayRoute(directionsService, directionsDisplay);
-    }); 
-
-}
-
-function calculateAndDisplayRoute(directionsService, directionsDisplay) {
-    var selectedMode = document.getElementById('mode').value;
-    directionsService.route({
-        origin: document.getElementById('origin').value, 
-        destination: document.getElementById('destination').value, 
-        travelMode: google.maps.TravelMode[selectedMode]
-    }, function (response, status) {
-        if (status == 'OK') {
-            directionsDisplay.setDirections(response);
-            // Mostra Distância:
-            document.getElementById('rtDistance').innerHTML =
-                "<strong>Distäncia:</strong> " + Math.round(response.routes[0].legs[0].distance.value / 1000) + " Km";
-            routeDistance = Math.round(response.routes[0].legs[0].distance.value / 1000);
-
-            // Mostra Tempo de viagem:
-            document.getElementById('rtTime').innerHTML =
-                "<strong>Tempo de viagem:</strong> " + Math.round(response.routes[0].legs[0].duration.value / 60) + " Minutos";
-            routeTime = Math.round(response.routes[0].legs[0].duration.value / 60);
-            if (routeTime > 20) {
-                    document.getElementById('urgencyDiv').style.display = "block";
-            }
-            else {
-                document.getElementById('urgencyDiv').style.display = "none";
-            }
-        } else {
-            window.alert('Requisição ao serviço do google falhou: ' + status);
-        }
-    });
-}
 
 /* Accordion -> Melhorar visual dos accordions. */
 
@@ -267,48 +131,6 @@ app.controller('Principal', ['$scope', '$compile', '$http', function ($scope, $c
     };
 
 }]); 
-
-// controller do histórico do motorista
-
-app.controller('DriverHistoryController', function ($scope, $http) {
-
-    $http.get('http://localhost:58255/api/v1/deliveries/driverhistory')
-        .then(function (response) {
-            $scope.deliveries = response.data;
-            console.log(response);
-        });
-
-    $scope.changePanicHtml = function (delivery) {
-        if (delivery.PanicPushed == true) {
-            var panicHtml = "Sim";
-            return panicHtml;
-        } else {
-            var panicHtml = "Não";
-            return panicHtml;
-        }
-    }
-})
-
-// controller do histórico do usuário
-
-app.controller('UserHistoryController', function ($scope, $http) {
-
-    $http.get('http://localhost:58255/api/v1/deliveries/userhistory')
-        .then(function (response) {
-            $scope.deliveries = response.data;
-            console.log(response);
-        });
-
-    $scope.changePanicHtml = function (delivery) {
-        if (delivery.PanicPushed == true) {
-            var panicHtml = "Sim";
-            return panicHtml;
-        } else {
-            var panicHtml = "Não";
-            return panicHtml;
-        }
-    }
-})
 
 // controller da nota do usuário
 
